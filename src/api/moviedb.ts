f7a4ff69f77c14b0cb634556404c81ee73d10be3
GabrielLopes001 <gabrieljob1@outlook.com>
2024-02-19 22:19:17 -0300

    add endpoints

--- src/api/moviedb.ts
+++ src/api/moviedb.ts
@@ -7,6 +7,22 @@ const apiBaseUrl = 'https://api.themoviedb.org/3/'
 const trendingMoviesEndPoint = `${apiBaseUrl}trending/movie/day?api_key=${apiKey}`
 const upcomingMoviesEndPoint = `${apiBaseUrl}/movie/upcoming?api_key=${apiKey}`
 const topRatedMoviesEndPoint = `${apiBaseUrl}/movie/top_rated?api_key=${apiKey}`
+const searchMovieEndPoint = `${apiBaseUrl}/search/movie?api_key=${apiKey}`
+
+const movieDetailsEndPoint = (id) =>
+  `${apiBaseUrl}/movie/${id}?api_key=${apiKey}`
+
+const movieCreditsEndPoint = (id) =>
+  `${apiBaseUrl}/movie/${id}/credits?api_key=${apiKey}`
+
+const similarMoviesEndPoint = (id) =>
+  `${apiBaseUrl}/movie/${id}/similar?api_key=${apiKey}`
+
+const personDetailsEndPoint = (id) =>
+  `${apiBaseUrl}/person/${id}?api_key=${apiKey}`
+
+const personMovies = (id) =>
+  `${apiBaseUrl}/person/${id}/movie_credits?api_key=${apiKey}`
 
 export const image500 = (path) =>
   path ? `https://image.tmdb.org/t/p/w500${path}` : null
@@ -20,7 +36,7 @@ type ApiCallProps = {
   params?: string
 }
 
-const apiCall = async ({ endpoint, params }: ApiCallProps) => {
+const apiCall = async ({ endpoint, params = [] }: ApiCallProps) => {
   const options = {
     method: 'GET',
     url: endpoint,
@@ -44,4 +60,22 @@ export const fetchUpcomingMovies = () => {
 }
 export const fetchTopRatedMovies = () => {
   return apiCall({ endpoint: topRatedMoviesEndPoint })
+}
+export const fetchMovieDetails = (id) => {
+  return apiCall({ endpoint: movieDetailsEndPoint(id) })
+}
+export const fetchMovieCredits = (id) => {
+  return apiCall({ endpoint: movieCreditsEndPoint(id) })
+}
+export const fetchSimilarMovies = (id) => {
+  return apiCall({ endpoint: similarMoviesEndPoint(id) })
+}
+export const fetchPersonDetails = (id) => {
+  return apiCall({ endpoint: personDetailsEndPoint(id) })
+}
+export const fetchPersonMovies = (id) => {
+  return apiCall({ endpoint: personMovies(id) })
+}
+export const fetchSearchMovie = (params) => {
+  return apiCall({ endpoint: searchMovieEndPoint, params })
 }
